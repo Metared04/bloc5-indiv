850ec7714c8b9dcd2df5c29972cddea9ce10e14b
Metared04 <e.metared@gmail.com>
2026-05-28 18:34:04 +0200

    Modification config.php

--- App/Config.php
+++ App/Config.php
@@ -7,36 +7,17 @@ namespace App;
  *
  * PHP version 7.0
  */
+
 class Config
 {
-
-    /**
-     * Database host
-     * @var string
-     */
-    const DB_HOST = 'localhost';
-
-    /**
-     * Database name
-     * @var string
-     */
-    const DB_NAME = 'videgrenierenligne';
-
-    /**
-     * Database user
-     * @var string
-     */
-    const DB_USER = 'webapplication';
-
-    /**
-     * Database password
-     * @var string
-     */
-    const DB_PASSWORD = '653rag9T';
-
-    /**
-     * Show or hide error messages on screen
-     * @var boolean
-     */
+    const DB_HOST = DB_HOST_VALUE;
+    const DB_NAME = DB_NAME_VALUE;
+    const DB_USER = DB_USER_VALUE;
+    const DB_PASSWORD = DB_PASSWORD_VALUE;
     const SHOW_ERRORS = true;
 }
+
+define('DB_HOST_VALUE', $_ENV['DB_HOST'] ?? getenv('DB_HOST') ?? 'localhost');
+define('DB_NAME_VALUE', $_ENV['DB_NAME'] ?? getenv('DB_NAME') ?? 'videgrenierenligne');
+define('DB_USER_VALUE', $_ENV['DB_USER'] ?? getenv('DB_USER') ?? 'webapplication');
+define('DB_PASSWORD_VALUE', $_ENV['DB_PASSWORD'] ?? getenv('DB_PASSWORD') ?? '653rag9T');
